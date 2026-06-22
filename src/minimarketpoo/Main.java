4965cbb13e798022a37a677f73ce1d4160b508b6
Rodolfo880 <rodolforodriguezculqui@gmail.com>
2026-06-22 07:32:02 -0500

    Main para inicializar listp

---
+++ src/minimarketpoo/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minimarketpoo;
+/**
+ *
+ * @author USUARIO
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login login = new Login();
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
+    }
+}
